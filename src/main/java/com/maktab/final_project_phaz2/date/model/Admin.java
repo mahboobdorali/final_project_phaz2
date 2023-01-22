5fab4feee42a62f9be4d3342e259b078ab6f88bf
mahboob <mahboobdorali@gmail.com>
2023-01-23 00:03:32 +0330

    create admin class

---
+++ src/main/java/com/maktab/final_project_phaz2/date/model/Admin.java
@@ -0,0 +1,8 @@
+package com.maktab.final_project_phaz2.date.model;
+import lombok.Data;
+@Data
+public class Admin {
+    private final String username = "admin";
+
+    private final String password = "1234";
+}
