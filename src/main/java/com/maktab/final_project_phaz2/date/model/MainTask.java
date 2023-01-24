6a8d02b6ba2402bbfcbc515615e3ebecab6ec10f
mahboob <mahboobdorali@gmail.com>
2023-01-24 14:08:09 +0330

    rename service class

--- src/main/java/com/maktab/final_project_phaz2/date/model/MainTask.java
+++ src/main/java/com/maktab/final_project_phaz2/date/model/MainTask.java
@@ -13,7 +13,7 @@ import lombok.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Service extends BaseEntity {
+public class MainTask extends BaseEntity {
 
     @Column(unique = true)
     private String name;
