fdc8fcd4ed0341bc515f77f52aae5b6278bf8e61
mahboob <mahboobdorali@gmail.com>
2023-01-24 14:01:24 +0330

    create two exception class

---
+++ src/main/java/com/maktab/final_project_phaz2/exception/DuplicateEntryException.java
@@ -0,0 +1,7 @@
+package com.maktab.final_project_phaz2.exception;
+
+public class DuplicateEntryException extends Exception{
+    public DuplicateEntryException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/maktab/final_project_phaz2/exception/NoResultException.java
@@ -0,0 +1,7 @@
+package com.maktab.final_project_phaz2.exception;
+
+public class NoResultException extends Exception{
+    public NoResultException(String message) {
+        super(message);
+    }
+}
