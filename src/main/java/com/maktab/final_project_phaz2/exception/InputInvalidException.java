2007ece78039a58bad07fa9897e4ab03ad5fa63c
mahboob <mahboobdorali@gmail.com>
2023-01-24 14:04:48 +0330

    create exception class

---
+++ src/main/java/com/maktab/final_project_phaz2/exception/InputInvalidException.java
@@ -0,0 +1,7 @@
+package com.maktab.final_project_phaz2.exception;
+
+public class InputInvalidException extends Exception{
+    public InputInvalidException(String message) {
+        super(message);
+    }
+}
