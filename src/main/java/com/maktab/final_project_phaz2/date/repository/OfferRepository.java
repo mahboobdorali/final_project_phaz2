abd86d45fd3b8181f21baaac7f20154e7f40d04e
mahboob <mahboobdorali@gmail.com>
2023-02-08 18:11:24 +0330

    add query to repository class

--- src/main/java/com/maktab/final_project_phaz2/date/repository/OfferRepository.java
+++ src/main/java/com/maktab/final_project_phaz2/date/repository/OfferRepository.java
@@ -4,6 +4,7 @@ import com.maktab.final_project_phaz2.date.model.Offer;
 import com.maktab.final_project_phaz2.date.model.OrderCustomer;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,7 +13,11 @@ import java.util.Optional;
 
 @Repository
 public interface OfferRepository extends JpaRepository<Offer, Long> {
-    List<Offer> findAllByOrdersCustomer(OrderCustomer orderCustomer, Sort sort);
+    @Query("select o from Offer o where o.orderCustomer=:orderCustomer order by o.priceOffer desc")
+    List<Offer> sortOfferByPriceOffer(OrderCustomer orderCustomer);
+
+    @Query("select o from Offer o where o.orderCustomer=:orderCustomer order by o.expert.averageScore desc")
+    List<Offer> sortOfferByScore(OrderCustomer orderCustomer);
 
     Optional<Offer> findById(Long id);
 
--- src/main/java/com/maktab/final_project_phaz2/date/repository/OrderRepository.java
+++ src/main/java/com/maktab/final_project_phaz2/date/repository/OrderRepository.java
@@ -2,8 +2,12 @@ package com.maktab.final_project_phaz2.date.repository;
 
 import com.maktab.final_project_phaz2.date.model.OrderCustomer;
 import com.maktab.final_project_phaz2.date.model.UnderService;
+import com.maktab.final_project_phaz2.date.model.enumuration.CurrentSituation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +16,9 @@ import java.util.Optional;
 public interface OrderRepository extends JpaRepository<OrderCustomer, Long> {
     Optional<OrderCustomer> findById(Long id);
     List<OrderCustomer>findAllByUnderService(UnderService underService);
+    @Transactional
+    @Query("select o from OrderCustomer o where o.underService.nameSubService=:nameSubService" +
+            " and o.currentSituation=:currentSituation or" +
+            " o.currentSituation=:currentSituation1")
+    List<OrderCustomer>findAllByUnderService(String nameSubService, CurrentSituation currentSituation, CurrentSituation currentSituation1);
 }
