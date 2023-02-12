25999f6ac34b2c03aa8b9e77aa75c77a986f6bf0
mahboob <mahboobdorali@gmail.com>
2023-02-12 21:15:38 +0330

    create dto class

---
+++ src/main/java/com/maktab/final_project_phaz2/date/dto/OpinionShowScoreDto.java
@@ -0,0 +1,13 @@
+package com.maktab.final_project_phaz2.date.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class OpinionShowScoreDto {
+
+    private int score;
+}
