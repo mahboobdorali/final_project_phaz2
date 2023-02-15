4763e48f5f6386636fab0305619d5c18a47706f6
mahboob <mahboobdorali@gmail.com>
2023-02-15 20:48:25 +0330

    add annotation for this class

--- src/main/java/com/maktab/final_project_phaz2/FinalProjectPhaz2Application.java
+++ src/main/java/com/maktab/final_project_phaz2/FinalProjectPhaz2Application.java
@@ -2,8 +2,12 @@ package com.maktab.final_project_phaz2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@ServletComponentScan
 public class FinalProjectPhaz2Application {
 
     public static void main(String[] args) {
