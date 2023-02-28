107c26f388bb316fbba9c59afe98218f7ab112c7
mahboob <mahboobdorali@gmail.com>
2023-02-28 22:55:25 +0330

    add method

--- src/main/java/com/maktab/final_project_phaz2/service/ImageService.java
+++ src/main/java/com/maktab/final_project_phaz2/service/ImageService.java
@@ -1,10 +1,13 @@
 package com.maktab.final_project_phaz2.service;
 
+import com.maktab.final_project_phaz2.date.model.Customer;
 import com.maktab.final_project_phaz2.date.model.Expert;
 import com.maktab.final_project_phaz2.date.model.Image;
+import com.maktab.final_project_phaz2.date.model.Person;
 import com.maktab.final_project_phaz2.date.repository.ImageRepository;
 import com.maktab.final_project_phaz2.exception.RequestIsNotValidException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -28,14 +31,11 @@ public class ImageService {
         expertService.updateExpert(expertById);
     }
 
-  /*  public Image findImageById(Long id) {
-        return imageRepository.findById(id).
-                orElseThrow(() -> new NoResultException("this image dose not exist"));
-    }*/
-
     @Transactional
-    public byte[] getImage(String emailAddress) {
-        Expert expertByEmail = expertService.findExpertByEmail(emailAddress);
-        return expertByEmail.getImage().getImageData();
+    public byte[] getImage() {
+        Expert expert = expertService.findExpertByEmail(((Person) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal()).getEmailAddress());
+        return expert.getImage().getImageData();
+
     }
 }
